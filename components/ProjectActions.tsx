028148b35d698600c9ef2fdd71e37227e44a966f
Jorge Matos <jisapjisap@gmail.com>
2023-07-22 00:04:50 +0200

    ProjectId

---
+++ components/ProjectActions.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+const ProjectActions = () => {
+  return (
+    <div>ProjectActions</div>
+  )
+}
+
+export default ProjectActions
